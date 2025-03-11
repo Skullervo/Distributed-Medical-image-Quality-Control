923ec65bbe9d40c6189d302693ec746db93fe899
Sampo Ylisiurua <110446843+Skullervo@users.noreply.github.com>
2025-03-11 12:47:33 +0200

    Update test_analyze_service.py

--- grpc_microservices/test_analyze_service.py
+++ grpc_microservices/test_analyze_service.py
@@ -1,31 +1,3 @@
-# import grpc
-# import analyze_service_pb2
-# import analyze_service_pb2_grpc
-
-
-# # 🔄 Yhdistetään Analyze Serviceen ja asetetaan viestikoko
-# options = [
-#     ("grpc.max_send_message_length", 200 * 1024 * 1024),
-#     ("grpc.max_receive_message_length", 200 * 1024 * 1024),
-# ]
-
-# channel = grpc.insecure_channel("localhost:50052", options=options)
-# stub = analyze_service_pb2_grpc.AnalyzeServiceStub(channel)
-
-# # 🔹 Vaihda tähän oikea `series_id`
-# series_id = "c7d9fd60-b23d3c7c-d12a4c99-d256e73f-148d9b44"
-
-# print(f"📡 Requesting analysis for series ID: {series_id}")
-# response = stub.AnalyzeDicomData(analyze_service_pb2.AnalyzeRequest(series_id=series_id))
-
-# # 🔍 Tarkistetaan vastaus
-# if response.message == "Analysis complete!":
-#     print(f"✅ Analyze onnistui sarjalle: {response.series_id}")
-# else:
-#     print(f"❌ Analyze epäonnistui: {response.message}")
-
-
-
 import grpc
 import analyze_service_pb2
 import analyze_service_pb2_grpc
